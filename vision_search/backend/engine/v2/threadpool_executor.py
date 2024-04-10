4363175fc1d9e69e5c2c1df6a0c5b700bbdc1c3f
YeonwooSung <neos960518@gmail.com>
2024-04-10 13:49:54 +0900

    feat: Implement CLIP based multimodal vision search engine

---
+++ vision_search/backend/engine/v2/threadpool_executor.py
@@ -0,0 +1,22 @@
+from concurrent.futures import ThreadPoolExecutor
+import numpy as np
+
+# custom module
+from .feature_extractor import CLIPFeatureExtractor
+
+class MultimodalThreadPoolEngine:
+    def __init__(self, model: str = "resnet34", device: str = "cpu", img_size: int = 224) -> None:
+        self.executor = ThreadPoolExecutor(max_workers=1)
+        self.feature_extractor = CLIPFeatureExtractor(model_path=model, device=device, img_size=img_size)
+
+    def run_engine_img(self, image: np.ndarray) -> np.ndarray:
+        return self.executor.submit(self.feature_extractor.get_image_features, image).result()
+
+    def run_engine_text(self, text: str) -> np.ndarray:
+        return self.executor.submit(self.feature_extractor.get_text_features, text).result()
+
+    async def arun_engine_img(self, images: np.ndarray) -> np.ndarray:
+        return await self.executor.submit(self.feature_extractor.get_image_features, images)
+
+    async def arun_engine_text(self, texts: str) -> np.ndarray:
+        return await self.executor.submit(self.feature_extractor.get_text_features, texts)
