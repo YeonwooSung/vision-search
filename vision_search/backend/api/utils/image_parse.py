60a9598aaa53cccaefbcf81976a3d2bf1f46bf84
YeonwooSung <neos960518@gmail.com>
2024-04-10 13:57:16 +0900

    feat: Add image utils

--- vision_search/backend/api/utils/image_parse.py
+++ vision_search/backend/api/utils/image_parse.py
@@ -1,5 +1,6 @@
 import tempfile
 from PIL import Image
+import smart_open
 from io import BytesIO
 import numpy as np
 
@@ -13,3 +14,11 @@ def save_as_tempfile(image):
 
 def load_image_into_numpy_array(data):
     return np.array(Image.open(BytesIO(data)))
+
+def pil_loader(image_file):
+    with Image.open(image_file) as image:
+        return image.convert("RGB")
+
+def load_image_from_url(image_url):
+    with smart_open.open(image_url, "rb") as image_file:
+        return pil_loader(image_file)
