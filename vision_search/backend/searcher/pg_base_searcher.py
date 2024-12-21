946d93ad118161ad5aa1ad4a2eda99176349eae3
YeonwooSung <neos960518@gmail.com>
2024-12-21 17:22:13 +0900

    feat: Start designing the schema of the image search BE

--- vision_search/backend/searcher/pg_base_searcher.py
+++ vision_search/backend/searcher/pg_base_searcher.py
@@ -1,18 +1,48 @@
 from pgvector.psycopg import register_vector
 import psycopg
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 import ssl
+
+# custom modules
+from backend.utils.singleton import Singleton
+
 
 # turning off SSL verification for custom certificates
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-class PostgresBaseSearcher:
-    def __init__(self):
-        pass
+class PostgresBaseSearcher(metaclass=Singleton):
+    def __init__(self, use_async: bool = False):
+        if use_async:
+            self.engine = create_async_engine("postgresql+asyncpg://postgres:password@localhost:5432/postgres")
+        else:
+            self.engine = create_engine("postgresql+psycopg2://postgres:password@localhost:5432/postgres")
+        self.use_async = use_async
 
-    def create_index(self):
-        pass
+        # register the vector type
+        register_vector(self.engine)
+
+    def _get_session(self):
+        return AsyncSession(self.engine) if self.use_async else self.engine.connect()
+
+
+    def create_index(self, create_table: bool = True):
+        session = self._get_session()
+        try:
+            if create_table:
+                session.execute('DROP TABLE IF EXISTS images')
+                session.execute('CREATE TABLE images (id bigserial PRIMARY KEY, embedding vector(512))')
+
+            session.execute('CREATE INDEX idx_images_embedding ON images USING ivfflat(embedding)')
+            session.commit()
+
+        except Exception as e:
+            print(f"Error creating index: {e}")
+        finally:
+            session.close()
+
 
     def bulk_ingest(self, generate_data, chunk_size=128):
         pass
